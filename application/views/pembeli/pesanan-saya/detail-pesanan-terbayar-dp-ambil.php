a7bee4dbe6097939790fc610a7fb577a579bb759
unknown <darifrahmanda@gmail.com>
2020-03-04 17:01:41 +0700

    html detail pesanan

--- application/views/pembeli/pesanan-saya/detail-pesanan-terbayar-dp-ambil.php
+++ application/views/pembeli/pesanan-saya/detail-pesanan-terbayar-dp-ambil.php
@@ -1,58 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
 
-  <!-- Twitter -->
-  <meta name="twitter:site" content="@themepixels">
-  <meta name="twitter:creator" content="@themepixels">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="Bracket">
-  <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
-  <meta name="twitter:image" content="http://themepixels.me/bracket/img/bracket-social.png">
-
-  <!-- Facebook -->
-  <meta property="og:url" content="http://themepixels.me/bracket">
-  <meta property="og:title" content="Bracket">
-  <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
-
-  <meta property="og:image" content="http://themepixels.me/bracket/img/bracket-social.png">
-  <meta property="og:image:secure_url" content="http://themepixels.me/bracket/img/bracket-social.png">
-  <meta property="og:image:type" content="image/png">
-  <meta property="og:image:width" content="1200">
-  <meta property="og:image:height" content="600">
-
-  <!-- Meta -->
-  <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
-  <meta name="author" content="ThemePixels">
-
-  <title>Detail Pesanan</title>
-
-  <!-- vendor css -->
-  <link href="../../materialize-css/dist/css/materialize.css" rel="stylesheet" media="screen,projection">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-  <link href="../../assets/lib/Ionicons/css/ionicons.css" rel="stylesheet">
-  <link href="../../assets/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
-</head>
-
-<body class="">
-  <header>
-    <nav class="top-nav" style="background-image: linear-gradient(to right, #00b7f8, #009dff, #0080ff, #005dff, #0029ee);">
-      <div class="container-fluid">
-        <div class="nav-wrapper">
-          <div class="row">
-            <div class="col s12 m10 offset-m1">
-              <a id="#" href="profil.html" onclick="return onBackKeyDown()" class="transparent" style="margin: auto 10px;"><span class="fa fa-chevron-left"></span></a>
-              <b class="header">&nbsp;Detail Pesanan</b>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
   <div class="container-fluid">
     <div class="row" style="margin-bottom: -30px">
       <div class="col s12">
@@ -122,26 +68,6 @@
     </div>
   </div>
 </div>
-</div>
-</div>
-
-<!-- ########## END: MAIN PANEL ########## -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
-<!-- <script type="text/javascript" src="../materialize-css/dist/js/materialize.js"></script> -->
-<!-- <script src="../assets/lib/popper.js/popper.js"></script> -->
-<!-- <script src="../assets/lib/bootstrap/bootstrap.js"></script> -->
-<script src="../../assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
-<script src="../../assets/lib/moment/moment.js"></script>
-<script src="../../assets/lib/jquery-ui/jquery-ui.js"></script>
-<script src="../../assets/lib/jquery-switchbutton/jquery.switchButton.js"></script>
-<script src="../../assets/lib/peity/jquery.peity.js"></script>
-<!-- <script src="../assets/lib/codemirror/addon/scroll/simplescrollbars.js"></script> -->
-
-<!-- <script src="../assets/js/bracket.js"></script> -->
-<script type="text/javascript" src="../../materialize-css/dist/js/materialize.js"></script>
-
-
-<script type="text/javascript" src="../../js/mobile_script.js"></script>
 <script type="text/javascript">
   $('.modal').modal();
   var DetailPesanan = storage.getItem("DetailPesanan");
